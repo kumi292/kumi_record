2ae3b6d875238a62131e77683e4fa35591051c3e
kumi292 <ryoyw.3531@gmail.com>
2025-11-17 17:00:32 +0900

    記事作成機能追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,23 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :editor)
   end
 end
